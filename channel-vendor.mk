10eef1fef0eec75943d5986f5bdfde13a76f6365
Nolen Johnson <johnsonnolen@gmail.com>
2025-03-15 02:08:57 -0400

    channel: Import repacked MotCamera
    Change-Id: Ie989f684a8e3ea40a8132ec4ce97e53a988e27c5

--- channel-vendor.mk
+++ channel-vendor.mk
@@ -191,5 +191,6 @@ PRODUCT_PACKAGES += \
     capi_v2_smartAmp_TAS25xx.so \
     tas2560_TI_0 \
     tas2560_TI_1 \
+    MotCamera2 \
     charge_only_mode \
     android.hardware.biometrics.fingerprint@2.1-fpcservice
